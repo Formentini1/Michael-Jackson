b21ecea33309db88ead152afc01867f9b336b7dc
Formentini1 <formentini.silva@escola.pr.gov.br>
2025-11-04 08:53:21 -0300

    up

---
+++ app22.js
@@ -0,0 +1,12 @@
+const lyricsContainer = document.getElementById("lyrics");
+const toggleButton = document.getElementById("toggleLyrics");
+
+const lyrics = `
+[Intro]
+Skin head, dead head
+Everybody gone bad
+Situation, aggravation
+Everybody allegation
+In the suit, on the news
+Everybody dog food
+
